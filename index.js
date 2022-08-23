d9d59f9257ccc0785525ccb305f9dfd0d2166d84
John Johnson Mbeba <john.mbeba@student.moringaschool.com>
2022-08-23 14:29:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+
+
+const submitData = (userName, userEmail) => {
+
+    const par = document.querySelector(".display-id");
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+        "Content-type" : "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name: userName,
+        email: userEmail
+    })
+    }).then(res => res.json()).then((data) => {
+        console.log(data);
+        par.textContent = data.id;
+    }).catch(err => {
+        par.textContent = err.message;
+    })
+}
